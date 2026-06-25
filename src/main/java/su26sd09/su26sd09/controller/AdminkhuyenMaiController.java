d5771c587279f89c32f383acedb48a780f93a93a
binhr2423 <susclmm@gmail.com>
2026-06-25 10:20:18 +0700

    update profiles customer

--- src/main/java/su26sd09/su26sd09/controller/AdminkhuyenMaiController.java
+++ src/main/java/su26sd09/su26sd09/controller/AdminkhuyenMaiController.java
@@ -59,40 +59,45 @@ public class AdminkhuyenMaiController {
 
     @PostMapping("/delete/{id}")
     public  String deleteKhuyenMai(@PathVariable("id") int id, Principal p){
-      if (CheckRole(p.getName())){
-          repo.delete(repo.findbyId(id));
+        if (CheckRole(p.getName())){
+            repo.delete(repo.findbyId(id));
 
-      }
+        }
         else{
             return "redirect:/admin/khuyen-mai";
-      }
-    return "redirect:/admin/khuyen-mai";
+        }
+        return "redirect:/admin/khuyen-mai";
     }
 
 
     @PostMapping("/save")
     public String saveKhuyenMai(RedirectAttributes redirect,Model model, Principal p, @ModelAttribute("khuyenMai") KhuyenMai m, BindingResult r){
         if(CheckRole(p.getName())){
-               if(r.hasErrors() || m.ngayBatDau == null || m.ngayKetThuc == null ){
-                    redirect.addFlashAttribute("error","vui lòng xem lại trường dữ liệu");
-                    return "redirect:/admin/khuyen-mai";
-               }
-
-               else if(m.ngayKetThuc.isBefore(m.ngayBatDau) || m.ngayKetThuc.equals(m.ngayBatDau)){
-                    redirect.addFlashAttribute("error","ngày kết thúc không phải sau ngày bắt đầu ít nhất 1 ngày");
-                  return "redirect:/admin/khuyen-mai";
-               }
-                if(m.giatriGiam.compareTo(BigDecimal.ZERO) <= 0){
-                    redirect.addFlashAttribute("error","giá trị giảm phải lớn hơn 0");
-                    return "redirect:/admin/khuyen-mai";
-                }
-                if(m.id == 0){
-                   redirect.addFlashAttribute("success","Luu khuyen mai thanh cong");
-
-                }if(m.id != 0){
-                    redirect.addFlashAttribute("success","Cap nhat khuyen mai thanh cong");
+            if(r.hasErrors() || m.ngayBatDau == null || m.ngayKetThuc == null ){
+                redirect.addFlashAttribute("error","vui lòng xem lại trường dữ liệu");
+                return "redirect:/admin/khuyen-mai";
             }
-               repo.save(m);
+
+            else if(m.ngayKetThuc.isBefore(m.ngayBatDau) || m.ngayKetThuc.equals(m.ngayBatDau)){
+                redirect.addFlashAttribute("error","ngày kết thúc không phải sau ngày bắt đầu ít nhất 1 ngày");
+                return "redirect:/admin/khuyen-mai";
+            }
+            if(m.giatriGiam.compareTo(BigDecimal.ZERO) <= 0){
+                redirect.addFlashAttribute("error","giá trị giảm phải lớn hơn 0");
+                return "redirect:/admin/khuyen-mai";
+            }
+            for (NguoiDung ng : nguoiDungRepo.getAll()){
+                if (ng.getEmail().equalsIgnoreCase(p.getName())){
+                    m.setN(ng);
+                }
+            }
+            if(m.id == 0){
+                redirect.addFlashAttribute("success","Luu khuyen mai thanh cong");
+
+            }if(m.id != 0){
+                redirect.addFlashAttribute("success","Cap nhat khuyen mai thanh cong");
+            }
+            repo.save(m);
 
             System.out.println(m.n.getEmail());
         }
@@ -102,26 +107,26 @@ public class AdminkhuyenMaiController {
 
     @GetMapping("/edit/{id}")
     public String updateKhuyenMai(Principal p,Model model,@PathVariable("id") int id){
-      if(CheckRole(p.getName())){
-       model.addAttribute("khuyenMai",repo.findbyId(id));
-       model.addAttribute("khuyenMais",repo.findAll());
+        if(CheckRole(p.getName())){
+            model.addAttribute("khuyenMai",repo.findbyId(id));
+            model.addAttribute("khuyenMais",repo.findAll());
 
-          model.addAttribute("nguoiDungs", nvRepo.ListAdd());
-       return "admin/khuyen-mai-list";
-    }
-    return "redirect:/admin/khuyen-mai";
+            model.addAttribute("nguoiDungs", nvRepo.ListAdd());
+            return "admin/khuyen-mai-list";
+        }
+        return "redirect:/admin/khuyen-mai";
     }
 
     @GetMapping("/search")
     public String search(Principal p ,Model model,@RequestParam("keyword") String keyword){
-     if(CheckRole(p.getName())){
-         model.addAttribute("khuyenMais", repo.findbyNameVoucher(keyword));
-         model.addAttribute("khuyenMai",new KhuyenMai());
+        if(CheckRole(p.getName())){
+            model.addAttribute("khuyenMais", repo.findbyNameVoucher(keyword));
+            model.addAttribute("khuyenMai",new KhuyenMai());
 
-         model.addAttribute("nguoiDungs", nvRepo.ListAdd());
-         return "admin/khuyen-mai-list";
-     }
+            model.addAttribute("nguoiDungs", nvRepo.ListAdd());
+            return "admin/khuyen-mai-list";
+        }
 
-     return "redirect:/admin/khuyen-mai";
+        return "redirect:/admin/khuyen-mai";
     }
 }
